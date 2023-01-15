be3f826359c315d5b19bdf158bd21165fa76976d
mxike <mike25092001@hotmail.com>
2023-01-15 12:58:56 +0100

    added .env file

--- telegram_run.py
+++ telegram_run.py
@@ -1,12 +1,15 @@
 from telethon import TelegramClient, events
-import logging
+from dotenv import load_dotenv
+import logging, os
+
+load_dotenv()
 
 logging.basicConfig(level=logging.WARNING)
 
-api_id = 12345
-api_hash = ''
-channel = [""]
-session_name = ''
+api_id = os.getenv('api_id')
+api_hash = os.getenv('api_hash')
+channel = [os.getenv('channel'), os.getenv('channel2')]
+session_name = os.getenv('session_name')
 
 client = TelegramClient(session_name, api_id, api_hash)
 client.start()
